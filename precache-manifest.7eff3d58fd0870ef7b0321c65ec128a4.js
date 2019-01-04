ded2172c9b46d916a17226a3b054a85158b422eb
droshev <mladen.droshev@sap.com>
2019-01-03 21:48:52 -0500

    Updates

---
+++ precache-manifest.7eff3d58fd0870ef7b0321c65ec128a4.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e678655fe64a8a126dd51619f22edde",
+    "url": "/droshev/fd-react-shellbar/static/media/72-Bold.1e678655.woff"
+  },
+  {
+    "revision": "486341b9c4f47a2bfbc2",
+    "url": "/droshev/fd-react-shellbar/static/css/main.119aa119.chunk.css"
+  },
+  {
+    "revision": "38fe47ce6d2c458734d4",
+    "url": "/droshev/fd-react-shellbar/static/js/1.38fe47ce.chunk.js"
+  },
+  {
+    "revision": "f34059dd60ac266362fc",
+    "url": "/droshev/fd-react-shellbar/static/js/runtime~main.f34059dd.js"
+  },
+  {
+    "revision": "6030ef457e70912083ef3154de684dc7",
+    "url": "/droshev/fd-react-shellbar/static/media/72-Italic.6030ef45.woff2"
+  },
+  {
+    "revision": "266fd3d74665316a027af275c204537d",
+    "url": "/droshev/fd-react-shellbar/static/media/72-BoldItalic.266fd3d7.woff2"
+  },
+  {
+    "revision": "0072d92bfeaf57f02de04342d06a3463",
+    "url": "/droshev/fd-react-shellbar/static/media/72-Regular.0072d92b.woff2"
+  },
+  {
+    "revision": "9b4487708ef140ace9dbc396a39bbf44",
+    "url": "/droshev/fd-react-shellbar/static/media/72-Italic.9b448770.woff"
+  },
+  {
+    "revision": "7f6c7cb94c0d8088b023553d46a64e07",
+    "url": "/droshev/fd-react-shellbar/static/media/72-Light.7f6c7cb9.woff2"
+  },
+  {
+    "revision": "1f13fc6d938b5cda6bde9229f722ea8c",
+    "url": "/droshev/fd-react-shellbar/static/media/72-BoldItalic.1f13fc6d.woff"
+  },
+  {
+    "revision": "56af1d33fbd8bf1d80afd53f3f1d01ca",
+    "url": "/droshev/fd-react-shellbar/static/media/72-CondensedBold.56af1d33.woff2"
+  },
+  {
+    "revision": "85e09d643e5d4b5f98c3aaf365bd1c02",
+    "url": "/droshev/fd-react-shellbar/static/media/72-Bold.85e09d64.woff2"
+  },
+  {
+    "revision": "c5f39800983eb3eb97ed2ba72a5be4b7",
+    "url": "/droshev/fd-react-shellbar/static/media/72-Condensed.c5f39800.woff"
+  },
+  {
+    "revision": "48cb3e07e7e509d6c81c420507d7916b",
+    "url": "/droshev/fd-react-shellbar/static/media/72-Regular.48cb3e07.woff"
+  },
+  {
+    "revision": "486341b9c4f47a2bfbc2",
+    "url": "/droshev/fd-react-shellbar/static/js/main.486341b9.chunk.js"
+  },
+  {
+    "revision": "15d69c7c6c46af899033b881f0786328",
+    "url": "/droshev/fd-react-shellbar/static/media/72-Condensed.15d69c7c.woff2"
+  },
+  {
+    "revision": "909707d4c2808b374157094c2950925e",
+    "url": "/droshev/fd-react-shellbar/static/media/72-CondensedBold.909707d4.woff"
+  },
+  {
+    "revision": "ab363ac31002ac1293658c71a84eef29",
+    "url": "/droshev/fd-react-shellbar/static/media/72-Light.ab363ac3.woff"
+  },
+  {
+    "revision": "c58be3ff685e0c820c8438bbf1579351",
+    "url": "/droshev/fd-react-shellbar/static/media/72-Italic.c58be3ff.ttf"
+  },
+  {
+    "revision": "c1ba0da78d7f6586cf5b00d5ef5eb806",
+    "url": "/droshev/fd-react-shellbar/static/media/SAP-icons.c1ba0da7.woff"
+  },
+  {
+    "revision": "a055c920fa3948666fc4412d72aa94f3",
+    "url": "/droshev/fd-react-shellbar/static/media/72-Regular.a055c920.ttf"
+  },
+  {
+    "revision": "317cde33cde273c643e2f4bfe788eefe",
+    "url": "/droshev/fd-react-shellbar/static/media/72-Bold.317cde33.ttf"
+  },
+  {
+    "revision": "e2f76695560366054ce11dc7efc046fd",
+    "url": "/droshev/fd-react-shellbar/static/media/72-Condensed.e2f76695.ttf"
+  },
+  {
+    "revision": "3ab665b6d2fbcc13822abf07520f3972",
+    "url": "/droshev/fd-react-shellbar/static/media/72-BoldItalic.3ab665b6.ttf"
+  },
+  {
+    "revision": "1abad19983bc369ee086769a8d503050",
+    "url": "/droshev/fd-react-shellbar/static/media/72-Light.1abad199.ttf"
+  },
+  {
+    "revision": "2bf8d215e6d4ac1b622728c4d2f06d4a",
+    "url": "/droshev/fd-react-shellbar/static/media/72-CondensedBold.2bf8d215.ttf"
+  },
+  {
+    "revision": "10d4e22e89161db3407efebe1b6c90f9",
+    "url": "/droshev/fd-react-shellbar/static/media/headshot-male.10d4e22e.jpg"
+  },
+  {
+    "revision": "537fa6901495bdfdf8510e56d2a82406",
+    "url": "/droshev/fd-react-shellbar/index.html"
+  }
+];
